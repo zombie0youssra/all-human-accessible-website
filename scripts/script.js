953da6a63f6f3115757a863225fb80cfd0beab31
youssra <112857270+zombie0youssra@users.noreply.github.com>
2022-11-03 09:58:04 +0100

    style changes en nieuwe div's

--- scripts/script.js
+++ scripts/script.js
@@ -10,30 +10,30 @@ function productScroll() {
   let item = document.getElementById("slide");
 
   for (let i = 0; i < next.length; i++) {
-    //refer elements by class name
+  
 
-    let position = 0; //slider postion
+    let position = 0; 
 
     prev[i].addEventListener("click", function() {
-      //click previos button
+    
       if (position > 0) {
-        //avoid slide left beyond the first item
+
         position -= 1;
-        translateX(position); //translate items
+        translateX(position); 
       }
     });
 
     next[i].addEventListener("click", function() {
       if (position >= 0 && position < hiddenItems()) {
-        //avoid slide right beyond the last item
+      
         position += 1;
-        translateX(position); //translate items
+        translateX(position);
       }
     });
   }
 
   function hiddenItems() {
-    //get hidden items
+    
     let items = getCount(item, false);
     let visibleItems = slider.offsetWidth / 210;
     return items - Math.ceil(visibleItems);
@@ -41,12 +41,12 @@ function productScroll() {
 }
 
 function translateX(position) {
-  //translate items
+  
   slide.style.left = position * -210 + "px";
 }
 
 function getCount(parent, getChildrensChildren) {
-  //count no of items
+
   let relevantChildren = 0;
   let children = parent.childNodes.length;
   for (let i = 0; i < children; i++) {
